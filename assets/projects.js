2699ae4b443a9757185d395a7e48d8208fc5c85d
Vausly Rust <172235138+Vausly@users.noreply.github.com>
2026-01-23 15:02:19 +0700

    react cloning to html

---
+++ assets/projects.js
@@ -0,0 +1,16 @@
+import{c as t,j as e}from"./index.js";/**
+ * @license lucide-react v0.344.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const i=t("Gamepad2",[["line",{x1:"6",x2:"10",y1:"11",y2:"11",key:"1gktln"}],["line",{x1:"8",x2:"8",y1:"9",y2:"13",key:"qnk9ow"}],["line",{x1:"15",x2:"15.01",y1:"12",y2:"12",key:"krot7o"}],["line",{x1:"18",x2:"18.01",y1:"10",y2:"10",key:"1lcuu1"}],["path",{d:"M17.32 5H6.68a4 4 0 0 0-3.978 3.59c-.006.052-.01.101-.017.152C2.604 9.416 2 14.456 2 16a3 3 0 0 0 3 3c1 0 1.5-.5 2-1l1.414-1.414A2 2 0 0 1 9.828 16h4.344a2 2 0 0 1 1.414.586L17 18c.5.5 1 1 2 1a3 3 0 0 0 3-3c0-1.545-.604-6.584-.685-7.258-.007-.05-.011-.1-.017-.151A4 4 0 0 0 17.32 5z",key:"mfqc10"}]]);/**
+ * @license lucide-react v0.344.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const o=t("Newspaper",[["path",{d:"M4 22h16a2 2 0 0 0 2-2V4a2 2 0 0 0-2-2H8a2 2 0 0 0-2 2v16a2 2 0 0 1-2 2Zm0 0a2 2 0 0 1-2-2v-9c0-1.1.9-2 2-2h2",key:"7pis2x"}],["path",{d:"M18 14h-8",key:"sponae"}],["path",{d:"M15 18h-5",key:"95g1m2"}],["path",{d:"M10 6h8v4h-8V6Z",key:"smlsk5"}]]);/**
+ * @license lucide-react v0.344.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const r=t("Video",[["path",{d:"m22 8-6 4 6 4V8Z",key:"50v9me"}],["rect",{width:"14",height:"12",x:"2",y:"6",rx:"2",ry:"2",key:"1rqjg6"}]]),c=()=>{const s=[{icon:e.jsx(o,{className:"w-8 h-8"}),title:"Vausly Post",description:"I write a blog to spend my time and share important news and tutorials about game, tech, internet related, or anything else.",link:"https://www.vausly.com"},{icon:e.jsx(r,{className:"w-8 h-8"}),title:"Video Creator",description:'Creating engaging & unique gaming content across <a href="https://bit.ly/VauslyCH">YouTube</a>, <a href="https://instagr.am/vausly">Instagram</a>, <a href="https://www.tiktok.com/@vauslyy">TikTok</a>, and <a href="https://fb.me/vausly">Facebook</a>.',link:"https://bio.vausly.com"},{icon:e.jsx(i,{className:"w-8 h-8"}),title:"Minecraft In-Game Content & Modding",description:"Creating a mods, maps, texture packs, & sharing seeds for Minecraft (discontinued since 2019). I will come back soon with something new. Check minecraft.vausly.com for more!",link:"https://minecraft.vausly.com"}];return e.jsx("div",{className:"grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8",children:s.map((a,n)=>e.jsxs("a",{href:a.link,target:"_blank",rel:"noopener noreferrer",className:"block bg-gradient-to-br from-purple-900/50 to-blue-900/50 p-6 rounded-xl hover:transform hover:scale-105 transition duration-300",children:[e.jsx("div",{className:"mb-4 text-purple-400",children:a.icon}),e.jsx("h3",{className:"text-xl font-bold mb-2",children:a.title}),e.jsx("p",{className:"text-gray-300",dangerouslySetInnerHTML:{__html:a.description}})]},n))})};export{c as default};
